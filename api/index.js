cbe0cc643b5756130beee81c657cb58d8921b2cf
Karan Bhatt <prakaranbhatt@gmail.com>
2024-06-23 22:41:33 +0545

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "../db/connect.js";
 
 const app = express();
+
 const port = 3000;
-app.listen(port, () => console.log(`Listening at port ${port}!`));
+
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Listening at port ${port}!...`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
---
+++ db/connect.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+const connectDB = (uri)=>{
+    mongoose.connect(uri);
+}
+
+export default connectDB;
